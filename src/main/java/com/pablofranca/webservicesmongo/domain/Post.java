de32b10308bc2e74351ec180850c273902ac5969
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-04-16 11:57:21 -0300

    Post entity]

---
+++ src/main/java/com/pablofranca/webservicesmongo/domain/Post.java
@@ -0,0 +1,86 @@
+package com.pablofranca.webservicesmongo.domain;
+
+import com.pablofranca.webservicesmongo.dto.AuthorDTO;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Document(collection = "post")
+public class Post implements Serializable {
+
+    @Id
+    private String id;
+    private LocalDate date;
+    private String title;
+    private String body;
+    private AuthorDTO author;
+
+    public Post(String id, LocalDate date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    public Post () {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public AuthorDTO getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDTO author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}
