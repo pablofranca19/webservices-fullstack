f098fe1b3aba0a4d00011ef4c5a166bfedbbd2fe
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-04-25 20:37:50 -0300

    Add serialVersionUID to DTO class

--- src/main/java/com/pablofranca/webservicesmongo/dto/AuthorDTO.java
+++ src/main/java/com/pablofranca/webservicesmongo/dto/AuthorDTO.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
 
 public record AuthorDTO(String id, String name) implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     public AuthorDTO (User user) {
         this(user.getId(), user.getName());
     }
