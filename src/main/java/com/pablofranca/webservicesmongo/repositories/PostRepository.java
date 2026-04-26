51522827a1694f764cb92818f125bf73c393839a
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-04-25 22:02:00 -0300

    Add new find post by title query method

--- src/main/java/com/pablofranca/webservicesmongo/repositories/PostRepository.java
+++ src/main/java/com/pablofranca/webservicesmongo/repositories/PostRepository.java
@@ -2,11 +2,16 @@ package com.pablofranca.webservicesmongo.repositories;
 
 import com.pablofranca.webservicesmongo.domain.Post;
 import com.pablofranca.webservicesmongo.domain.User;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+
+    List<Post> findPostsByTitleIgnoreCase(String title);
 
 }
