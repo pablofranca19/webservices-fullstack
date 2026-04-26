23d1a644dc023c45e7f33b8d18eb5001f0445d7f
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-04-25 22:03:03 -0300

    Add find post by title method

--- src/main/java/com/pablofranca/webservicesmongo/services/PostService.java
+++ src/main/java/com/pablofranca/webservicesmongo/services/PostService.java
@@ -5,6 +5,9 @@ import com.pablofranca.webservicesmongo.repositories.PostRepository;
 import com.pablofranca.webservicesmongo.services.exception.IdNotFoundException;
 import com.pablofranca.webservicesmongo.services.exception.ObjectNotFoundException;
 import com.pablofranca.webservicesmongo.services.exception.PostNotFoundException;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.CriteriaDefinition;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,4 +29,9 @@ public class PostService {
     public Optional<Post> findById (String id) {
         return Optional.of(postRepository.findById(id).orElseThrow(() -> new PostNotFoundException("No post found with id " + id)));
     }
+
+    public List<Post> findByTitle (String text) {
+        return postRepository.findPostsByTitleIgnoreCase(text);
+    }
+
 }
