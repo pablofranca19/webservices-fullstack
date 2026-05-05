79d63c1358c55ecc7a800fb14ae62c9de6ddd769
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-05-04 21:59:48 -0300

    Add new username search through an endpoint

--- src/main/java/com/pablofranca/webservicesmongo/repositories/UserRepository.java
+++ src/main/java/com/pablofranca/webservicesmongo/repositories/UserRepository.java
@@ -4,8 +4,11 @@ import com.pablofranca.webservicesmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
+    List<User> findUserByNameIgnoreCase(String name);
 
 }
