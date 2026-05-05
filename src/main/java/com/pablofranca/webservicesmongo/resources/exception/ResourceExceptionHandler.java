145d43fbfe8de23464bcd6b2c97ea3568f5d5d4c
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-05-04 22:02:10 -0300

    Add exception handling for posts

--- src/main/java/com/pablofranca/webservicesmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/pablofranca/webservicesmongo/resources/exception/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pablofranca.webservicesmongo.resources.exception;
 
 
 import com.pablofranca.webservicesmongo.services.exception.ObjectNotFoundException;
+import com.pablofranca.webservicesmongo.services.exception.PostNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,15 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(Instant.now(), status.value(), "Object not found", exception.getMessage(), servletRequest.getRequestURI());
         return ResponseEntity.status(status).body(error);
     }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity <StandardError> postNotFoundException (PostNotFoundException e, HttpServletRequest servletRequest) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(Instant.now(), status.value(), "Post not found", e.getMessage(), servletRequest.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+
 
 
 }
